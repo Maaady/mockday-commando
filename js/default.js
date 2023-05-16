4bb469d9d8c17e85c1cc35b0b9084b4439c67083
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-05-16 15:34:04 -0300

    Update default.js
    Only checks for navigator's user language. Nothing else.

--- js/default.js
+++ js/default.js
@@ -1,8 +1,8 @@
 // Gets the user's preferred language
-let userLang = navigator.language || navigator.userLanguage;
+let userLang = navigator.userLanguage;
 
 // Redirects the user to the website version in their preferred language
-if (userLang === "en-us") {
+if (userLang === "en-US") {
     window.location.href = "https://hceresetti.github.io/mockdayCommando/index.html";
 } else {
     window.location.href = "https://hceresetti.github.io/mockdayCommando/indice.html";
