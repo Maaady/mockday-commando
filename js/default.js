8848cefa34d4fada4fd0ee304bfbcf3f0cc13547
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-05-16 16:57:22 -0300

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,9 +1,9 @@
 // Obtém o idioma preferido do usuário
-let userLang = navigator.language || navigator.userLanguage;
+let userLang = navigator.language || navigator.userLanguage; 
 
 // Redireciona o usuário para a versão do sítio eletrônico em seu idioma preferido
-if (userLang === "pt-BR") {
-    window.location.href = "https://hceresetti.github.io/mockdaycommando-br/";
-} else {
-    window.location.href = "https://hceresetti.github.io/mockdaycommando/";
+if (userLang === "pt-BR" && !window.location.href.includes("/pt-BR")) {
+    window.location.href = "https://hceresetti.github.io/mockdaycommando-br";
+} else if (!window.location.href.includes("/en-US")) {
+    window.location.href = "https://hceresetti.github.io/mockdaycommando";
 }
