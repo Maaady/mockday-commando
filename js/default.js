03cbba52d7c07392a186faacb66d3a3bb86d78f9
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-05-16 15:29:11 -0300

    Update default.js
    userLang's value changed from "en-US" to "en-us".

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@
 let userLang = navigator.language || navigator.userLanguage;
 
 // Redirects the user to the website version in their preferred language
-if (userLang === "en-US") {
+if (userLang === "en-us") {
     window.location.href = "https://hceresetti.github.io/mockdayCommando/index.html";
 } else {
     window.location.href = "https://hceresetti.github.io/mockdayCommando/indice.html";
