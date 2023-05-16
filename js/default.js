3485f85bc7ecf7727da7ef04fb9ac6be719cd57d
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-05-16 16:32:27 -0300

    Brazilian repository created to fix lang switching
    Maintaining two "index" HTML files are unnecessary since we have now two repositories dedicated to each page language set, both English (US) and Portuguese (Brazil)

--- js/default.js
+++ js/default.js
@@ -1,9 +1,9 @@
-// Gets the user's preferred language
+// Obtém o idioma preferido do usuário
 let userLang = navigator.language || navigator.userLanguage;
 
-// Redirects the user to the website version in their preferred language
-if (userLang === "en-US") {
-    window.location.href = "https://hceresetti.github.io/mockdaycommando/";
+// Redireciona o usuário para a versão do sítio eletrônico em seu idioma preferido
+if (userLang === "pt-BR") {
+    window.location.href = "https://hceresetti.github.io/mockdaycommando-br/";
 } else {
     window.location.href = "https://hceresetti.github.io/mockdaycommando/";
 }
