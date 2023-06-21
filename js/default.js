2c1671abb939fd20a2985015f6ab00722e515f17
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-06-21 19:43:56 -0300

    Delete default.js
    Delete default.js as Mockday Commando services were only operated in Brazil, so there is no need to change user language as all of its customers are Brazilian.

--- js/default.js
+++
@@ -1,9 +0,0 @@
-// Obtém o idioma preferido do usuário
-let userLang = navigator.language || navigator.userLanguage; 
-
-// Redireciona o usuário para a versão do sítio eletrônico em seu idioma preferido
-if (userLang === "pt-BR" && !window.location.href.includes("/mockdaycommando-br")) {
-    window.location.href = "https://hceresetti.github.io/mockdaycommando-br";
-} else if (userLang === "en-US" && !window.location.href.includes("/mockdaycommando")) {
-    window.location.href = "https://hceresetti.github.io/mockdaycommando";
-}
